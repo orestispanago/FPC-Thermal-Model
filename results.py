9b79152276c474d46fd1e40812e9acde5e9b1dad
orestispanago <orestispanago@gmail.com>
2022-02-23 15:56:12 +0200

    Using GlassCover class for temperature calculation

--- results.py
+++ results.py
@@ -1,7 +1,10 @@
 import time
+from tabnanny import check
+from tkinter import N
 
 import numpy as np
 
+from calculations import GlassCover
 from coefficients import coeff
 from output import save_to_file
 from plotting import plots
@@ -32,7 +35,7 @@ start = time.time()
 
 t_amb = np.zeros(n_time_steps + 1) + (28+273.15)  # Ambient temp.
 G_r = np.zeros(n_time_steps + 1)  # Heat flux of solar radiation. (W/sqm)
-t_glass = np.ones(n_nodes) * 293  # initial glass temp.
+# t_glass = np.ones(n_nodes) * 293  # initial glass temp.
 t_air = np.ones(n_nodes) * 293    # initial air gap temp.
 t_abs = np.ones(n_nodes) * 293    # initial absorber temp.
 t_water = np.ones(n_nodes) * 293  # initial fluid temp.
@@ -54,10 +57,13 @@ def step_down(t, trigger_time):
     return 0
 
 
-def check_convergence(n_nodes, n_converge):
+glass = GlassCover(n_nodes, 293)
+
+
+def check_convergence(n_nodes, n_converge, glass):
     for j in range(n_nodes):
         error = np.zeros(5)
-        error[0] = abs(t_glass[j] - t_glass_old[j]) / t_glass[j]
+        error[0] = glass.check_converge(j)
         error[1] = abs(t_air[j] - t_air_old[j]) / t_air[j]
         error[2] = abs(t_abs[j] - t_abs_old[j]) / t_abs[j]
         error[3] = abs(t_water[j] - t_water_old[j]) / t_water[j]
@@ -70,16 +76,16 @@ def check_convergence(n_nodes, n_converge):
     return n_converge
 
 
-def calc_t_glass(i, t):
-    return ((t_glass_old[i] / dtau) + (B[i] * t_amb[t]) + (C[i] * t_abs[i]) + (D[i] * t_air[i]) + (E[i] * G_r[t])) / F[i]
+# def calc_t_glass(i, t):
+#     return ((t_glass_old[i] / dtau) + (B[i] * t_amb[t]) + (C[i] * t_abs[i]) + (D[i] * t_air[i]) + (E[i] * G_r[t])) / F[i]
 
 
-def calc_t_air(i, t):
-    return ((t_air_old[i] / dtau) + (G[i] * (t_glass[i] + t_abs[i]))) / H[i]
+def calc_t_air(i, t, glass):
+    return ((t_air_old[i] / dtau) + (G[i] * (glass.t_glass[i] + t_abs[i]))) / H[i]
 
 
-def calc_t_abs(i, t):
-    return ((t_abs_old[i] / dtau) + (K[i] * G_r[t]) + (L[i] * t_glass[i]) + (M[i] * t_air[i]) + (O[i] * t_water[i]) + (P[i] * t_insul[i])) / Q[i]
+def calc_t_abs(i, t, glass):
+    return ((t_abs_old[i] / dtau) + (K[i] * G_r[t]) + (L[i] * glass.t_glass[i]) + (M[i] * t_air[i]) + (O[i] * t_water[i]) + (P[i] * t_insul[i])) / Q[i]
 
 
 def calc_t_insul(i, t):
@@ -94,29 +100,30 @@ for t in range(n_time_steps):
     n_converge = 0
     G_r[t] = 660*step_down(t*dtau, 3600)
     B, C, D, E, F, G, H, K, L, M, O, P, Q, R, S, U, V, W, X = coeff(
-        t_glass, t_air, t_abs, t_water, t_insul, t_amb, dtau, dz, n_nodes, mdot, t, w_f)
+        glass.t_glass, t_air, t_abs, t_water, t_insul, t_amb, dtau, dz, n_nodes, mdot, t, w_f)
     n_iter = 0
     while n_converge < 5 * n_nodes:
         n_iter = n_iter + 1
-        t_glass_old = t_glass.copy()
+        # t_glass_old = t_glass.copy()
+        glass.update_old()
         t_air_old = t_air.copy()
         t_abs_old = t_abs.copy()
         t_water_old = t_water.copy()
         t_insul_old = t_insul.copy()
 
-        t_glass[0] = calc_t_glass(0, t)
-        t_air[0] = calc_t_air(0, t)
-        t_abs[0] = calc_t_abs(0, t)
+        glass.calc_t(0, t, dtau, t_amb, t_abs, t_air, G_r, B, C, D, E, F)
+        t_air[0] = calc_t_air(0, t, glass)
+        t_abs[0] = calc_t_abs(0, t, glass)
         t_insul[0] = calc_t_insul(0, t)
         t_water[0] = t_in[t]
         for j in range(1, n_nodes):
-            t_glass[j] = calc_t_glass(j, t)
-            t_air[j] = calc_t_air(j, t)
-            t_abs[j] = calc_t_abs(j, t)
+            glass.calc_t(j, t, dtau, t_amb, t_abs, t_air, G_r, B, C, D, E, F)
+            t_air[j] = calc_t_air(j, t, glass)
+            t_abs[j] = calc_t_abs(j, t, glass)
             t_water[j] = calc_t_water(j)
             t_insul[j] = calc_t_insul(j, t)
-        n_converge = check_convergence(n_nodes, n_converge)
-    t_glass_node[t] = t_glass[selected_node]
+        n_converge = check_convergence(n_nodes, n_converge, glass)
+    t_glass_node[t] = glass.t_glass[selected_node]
     t_air_node[t] = t_air[selected_node]
     t_abs_node[t] = t_abs[selected_node]
     t_water_node[t] = t_water[selected_node]
